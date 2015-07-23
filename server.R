4b2d3bb175e106219db8a2d646fcc95a35de33f7
Alberto Miedes Garcés <albertomg994@gmail.com>
2015-07-23 12:24:19 +0200

    Primera versión funcional

--- server.R
+++ server.R
@@ -7,48 +7,73 @@ library(readr)
 
 shinyServer(function(input, output) {
   
-  # Dataframe de ejemplo 1
-  # ---------------------------------------------
-  df_example1 <- data.frame(location = c("White House", "Impound Lot", "Bush Garden", "Rayburn", "Robertson House", "Beers Elementary"), latitude = c(38.89710, 38.81289, 38.94178, 38.8867787, 38.9053894, 38.86466), longitude = c(-77.036545, -77.0171983, -77.073311, -77.0105317, -77.0616441, -76.95554))
   
-  # Dataframe de ejemplo 2
-  # ---------------------------------------------
-  location <- c("Evento1", "Evento2", "Evento3", "Evento4")
-  #date <- as.Date(c("2015-7-21", "2015-7-22", "2015-7-23", "2015-7-24"))
-  latitude = c(40.39227092120195, 40.39241367848808, 40.49082275610172, 40.42834456035844)
-  longitude = c(-3.684968648157021, -3.697259467865366, -3.7216054330053647, -3.728941931668392)
-  df_example2 <- data.frame(location, latitude, longitude)
+  # Lectura de los datos
+  datos <- read.csv(file="data/datos1.csv", header=TRUE, sep=";")
   
-  # Dataframe de ejemplo 3
-  # ---------------------------------------------
-  #df_example3 <- xmlToDataFrame("data/datos.xml")
-  #df_example4 <- read_csv("data/datos.csv")
-  #df_example5 <- read.csv(file="data/datos.csv", header=TRUE, sep=";", colClasses = c("NULL", "character", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "numeric", "numeric", "NULL", "NULL"))
-  df_example5 <- read.csv(file="data/datos1.csv", header=TRUE, sep=";")
   
+  # GRÁFICAS SOBRE LOS DATOS
+  # ==========================================================================================
+  # Reparto por distritos
+  distritos <- datos$DISTRITO
+  rsm_distritos <- summary(distritos)
+  output$plotDistritos <- renderPlot({
+    barplot(rsm_distritos, 
+        main="Distribución por distritos",
+        ylab="Nº actividades",
+        xlab="Distrito")
+  })
+  
+  # Gratuidad o no
+  gratuidad <- datos$GRATUITO # Esto va regular
+  rsm_gratuidad <- summary(gratuidad)
+  
+  # Fechas
+  fechas <- datos$FECHA
+  rsm_fechas <- summary(fechas)
+  
+  # Horas
+  horas <- datos$HORA
+  rsm_horas <- summary(horas)
+  output$plotHoras <- renderPlot({
+    barplot(rsm_horas, 
+            main="Distribución por horas",
+            ylab="Nº actividades",
+            xlab="Hora")
+  })
+  
+  # MAPA
+  # ==========================================================================================
   # Construir el mapa
-  # ---------------------------------------------
   map <- leaflet()
   map <- addTiles(map)
   #map %>% setView(40.89710, -3.63655, 1)
   
-  # Asociamos que df cargamos
-  # ---------------------------------------------
-  df_example <- df_example5
-  n_elems <- nrow(df_example5)
-  
   # Poblar el mapa
-  # ---------------------------------------------
-  for (i in 1:n_elems) {
-    map <- addMarkers(map, lng=df_example$longitude[i], lat=df_example$latitude[i], popup=df_example$NOMBRE[i])
+  for (i in 1:nrow(datos)) {
+    map <- addMarkers(map, lng=datos$longitude[i], lat=datos$latitude[i], popup=datos$NOMBRE[i])
   }
   
-  # Renderizamos el mapa
-  # ---------------------------------------------
+  # RenderizaR el mapa
   output$myMap <- renderLeaflet({
     #map <- addMarkers(map, lng=input$longitud, lat=input$latitud, popup="The birthplace of R")
     map
   })
   
+  # TABLA DE DATOS
+  # ==========================================================================================
+  # Renderizar la tabla a través del data.frame
+  output$tabla <- DT::renderDataTable(DT::datatable({
+    data <- datos
+    data <- subset(datos, select = c(NOMBRE, GRATUITO, FECHA, HORA, NOMBRE.INSTALACION))
+    
+    if (input$distrito != "All") {
+      data <- data[data$DISTRITO == input$distrito,]
+    }
+    if (input$instalacion != "All") {
+      data <- data[data$NOMBRE.INSTALACION == input$instalacion,]
+    }
+    data
+  }))
 }
 )
--- ui.R
+++ ui.R
@@ -4,23 +4,58 @@ library(sp)
 library(rgdal)
 library(readr)
 
-shinyUI(fluidPage(
-	# Creamos un panel de título
-	titlePanel("Title Panel"),
+shinyUI(
+  navbarPage("Nobre de la barra",
+    # Tab 1
+    tabPanel("Mapa",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("plotType", "Plot type", c("Scatter"="p", "Line"="l"))
+        ),
+        
+        mainPanel(
+          h2("Mapa con las ubicaciones"),
+          leafletOutput("myMap")
+        )
+      )
+      # ./sidebarLayout
+    ),
+    # ./ tabPanel
+    
+    # Tab 2
+    tabPanel("Tabla de datos",
+      # Row para filtros
+      fluidRow(
+        column(5, offset = 1,
+          selectInput("distrito", "Distrito:", c("All", unique(as.character(datos$DISTRITO))))
+        ),
+        column(5,
+         	selectInput("instalacion", "Nombre instalaciones:", c("All", unique(as.character(datos$NOMBRE.INSTALACION))))
+        )
+      ),
 
-	# Creamos un panel lateral (siempre recibe estos dos argumentos)
-	sidebarLayout(
-		# Que contiente a su vez otros dos paneles
-		sidebarPanel(
-		    h3("Coordenadas:"),
-		    numericInput("latitud", label = p("Latitud"), value = 1),
-		    numericInput("longitud", label = p("Longitud"), value = 1)
-	  	),
-
-		# Panel principal
-		mainPanel("main panel izq.",
-			h1("Titulo 2"),
-			leafletOutput("myMap")
-		)
-	)
-))
+      # La tabla en si
+      fluidRow(
+         column(10, offset = 1,
+         	  DT::dataTableOutput("tabla")
+         )
+      )
+    ),
+    # ./ tabPanel
+  
+    # Tab 3
+    tabPanel("Estadisticas",
+     fluidRow(
+       column(5, offset = 1,
+          plotOutput('plotDistritos')
+       ),
+       column(5,
+          plotOutput('plotHoras')
+       )
+     )
+    )
+    # ./ tabPanel
+  )
+  # ./navbarPage()
+)
+# ./shinyUI()
